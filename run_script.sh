ddb22a08747ce837ae9df7e854bbc95702bd93da
Josep Lluís Berral García <josepllberral@gmail.com>
2018-02-13 18:19:50 +0100

    Simplified run script to only one

--- run_script.sh
+++ run_script.sh
@@ -8,24 +8,29 @@
 
 # THIS CREATES A SINGLE-NODE SPARK CLUSTER IN NORD3
 # NORD3 NODES -> 16 CORES; 127GB RAM
-# DEFAULT WORKER (ALL) -> 16 CORES; 125GB RAM
+# DEFAULT SPARK WORKER (ALL RESOURCES) -> 16 CORES; 125GB RAM
 
 ###############################################################################
 ## EXPERIMENT PREPARATION
 
 # COPY THE EXPERIMENT FILES INTO WORK DIRECTORY
-HOME_DIR=/gpfs/home/bsc31/bsc31553/spark-test
-WORK_DIR=/gpfs/scratch/bsc31/bsc31553/spark-test
+HOME_DIR=/gpfs/home/bscXX/bscXXXXX/spark-test
+WORK_DIR=/gpfs/scratch/bscXX/bscXXXXX/spark-test
 
 mkdir -p $WORK_DIR
-cp $HOME_DIR/example.scala $WORK_DIR/example.R
+cd $WORK_DIR
+
+# SPARK SCRIPT TO BE SUBMITTED (SCALA, PYTHON OR R)
+EXPERIMENT=example.scala
+#EXPERIMENT=example.py
+#EXPERIMENT=example.R
+
+# COPY EXPERIMENT AND DATA FILES
 cp $HOME_DIR/mobydick.txt $WORK_DIR/mobydick.txt
+cp $HOME_DIR/$EXPERIMENT $WORK_DIR/$EXPERIMENT
 if [ ! -d $WORK_DIR/dcc-spark01.simg ]; then
 	cp $HOME_DIR/dcc-spark01.simg $WORK_DIR/dcc-spark01.simg;
 fi
-cd $WORK_DIR
-
-EXPERIMENT=example.R
 
 ###############################################################################
 ## SPARK CONFIGURATION (YOU PROBABLY DON'T NEED TO TOUCH THIS)
@@ -82,7 +87,6 @@ r)
 esac
 singularity exec dcc-spark01.simg $SHELL --master spark://$HOSTNAME:7077 < $EXPERIMENT ;
 
-
 # KILL SPARK SESIONS
 echo "Cleaning Spark Nodes"
 H_LIST_REV=`for i in $H_LIST; do echo $i; done | tac`
@@ -93,15 +97,17 @@ done;
 ###############################################################################
 ## EXPERIMENT WRAP-UP
 
-# CREATE HOME EXPERIMENT FOLDER (AND COPY SPARK OUTPUT)
+# CREATE HOME EXPERIMENT FOLDER
 mkdir -p $HOME_DIR/experiment_${LSB_JOBID}
+
+# COPY SPARK OUTPUT BACK TO HOME
 mv $WORK_DIR/${MASTER_OUT}* $HOME_DIR/experiment_${LSB_JOBID}/
 mv $WORK_DIR/${WORKER_OUT}* $HOME_DIR/experiment_${LSB_JOBID}/
 
 # COPY RESULTS BACK TO HOME
 mv $WORK_DIR/wc-result.data $HOME_DIR/experiment_${LSB_JOBID}/
-mv $WORK_DIR/example.R $HOME_DIR/experiment_${LSB_JOBID}/
 mv $WORK_DIR/mobydick.txt $HOME_DIR/experiment_${LSB_JOBID}/
+mv $WORK_DIR/$EXPERIMENT $HOME_DIR/experiment_${LSB_JOBID}/
 
 # THAT'S ALL, FOLKS!
 echo "Bye!"
